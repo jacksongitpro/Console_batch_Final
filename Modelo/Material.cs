36e8201b5ccd1e034c92bd319e663963e2e73802
Jackson Garcia <jacksongarcia101@gmail.com>
2021-07-18 22:56:14 -0300

    Add project files.

---
+++ Modelo/Material.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_batch_Final.Modelo
+{
+    public class Material
+    {
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+
+    }   
+        
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console_batch_Final.Modelo;
+
+namespace Console_batch_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base.CargarReporte();
+            Base.CalcularStock();
+            //Base.CalcularMenorStock();
+            //Base.MostrarMensaje();
+
+
+            Console.WriteLine($"El material de menor stock es: {Base.Stock[0].Nombre} con {Base.Stock[0].Cantidad} unidades");
+            Console.WriteLine($"El material de Mayor stock es: {Base.Stock[1].Nombre} con {Base.Stock[1].Cantidad} unidades");
+            Console.ReadLine();
+
+
+
+
+
+        }
+        
+    }
+}
